0ef052015c50668653843de2843422506da3545e
Jschong122 <cilmbv03918@gmail.com>
2024-09-04 03:13:43 +0800

    Integrate medical-system into main repository

---
+++ frontend/medical-system/app/layout.js
@@ -0,0 +1,17 @@
+import { Comfortaa } from "next/font/google";
+import "./globals.css";
+
+const comfortaa = Comfortaa({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Medical System",
+  description: "Medical System",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={comfortaa.className}>{children}</body>
+    </html>
+  );
+}
